2d3b4583652299adbe11e711551c6924c513fd06
Haseeb Heaven <haseeb_heaven@yahoo.com>
2022-05-21 15:33:06 +0400

    Updated Translator AVLTree

--- Translator.py
+++ Translator.py
@@ -9,63 +9,63 @@ from Result import Result
 # AVLTree Implementation #
 ##########################
 
-#class Translator:
-#    """ AVLTree Implementation """
+class Translator:
+   """ AVLTree Implementation """
 
-#    def __init__(self):
-#        time1 = datetime.now()
-#        print("Loading Database...")
-#        self.__words = [line.rstrip('\r\n') for line in open("Database\\UltimateZeroDict.txt", "r", encoding='utf-8')]
-#        self.__tree = self.__getDictionary()
-#        self.SpellChecker = self.__makeSpellChecker()
-#        time2 = datetime.now()
-#        print("Loaded in {0}\r\n".format(time2 - time1))
+   def __init__(self):
+       time1 = datetime.now()
+       print("Loading Database...")
+       self.__words = [line.rstrip('\r\n') for line in open("Database\\UltimateZeroDict.txt", "r", encoding='utf-8')]
+       self.__tree = self.__getDictionary()
+       self.SpellChecker = self.__makeSpellChecker()
+       time2 = datetime.now()
+       print("Loaded in {0}\r\n".format(time2 - time1))
 
-#    def __getDictionary(self):
-#        words = [line.rstrip('\r\n') for line in open("Database\\UltimateZeroDict.txt", "r", encoding='utf-8')]
-#        avl = AVLTree()
-#        for word in self.__words:
-#            temp = word.lower().split(':')
-#            avl.insert(temp[0], temp[1])
-#        return avl
+   def __getDictionary(self):
+       words = [line.rstrip('\r\n') for line in open("Database\\UltimateZeroDict.txt", "r", encoding='utf-8')]
+       avl = AVLTree()
+       for word in self.__words:
+           temp = word.lower().split(':')
+           avl.insert(temp[0], temp[1])
+       return avl
 
-#    def __makeSpellChecker(self):
-#        words = [line.rstrip('\r\n') for line in open("Database\\UltimateZeroDict.txt", "r", encoding='utf-8')]
-#        english = []
-#        for word in self.__words:
-#            temp = word.lower().split(':')
-#            english.append(temp[0])
-#        return SpellChecker(english)
+   def __makeSpellChecker(self):
+       words = [line.rstrip('\r\n') for line in open("Database\\UltimateZeroDict.txt", "r", encoding='utf-8')]
+       english = []
+       for word in self.__words:
+           temp = word.lower().split(':')
+           english.append(temp[0])
+       return SpellChecker(english)
 
-#    def hasWord(self, englishWord):
-#        return self.__tree.has_key(englishWord.lower())
+   def hasWord(self, englishWord):
+       return self.__tree.has_key(englishWord.lower())
 
-#    def Translate(self, englishWord):
-#        englishWord = englishWord.lower()
-#        trans = self.__tree.ValueOf(englishWord)
-#        result = Result()
-#        if trans is not None:
-#            result.Value = trans
-#        else:
-#            result.Suggesions = self.SpellChecker.GetSuggestions(englishWord)
-#        return result
+   def Translate(self, englishWord):
+       englishWord = englishWord.lower()
+       trans = self.__tree.ValueOf(englishWord)
+       result = Result()
+       if trans is not None:
+           result.Value = trans
+       else:
+           result.Suggesions = self.SpellChecker.GetSuggestions(englishWord)
+       return result
     
-#    def TranslateSentense(self, sentense):
-#        words = sentense.split(' ')
-#        output = []
-#        wrong = "Unable to recognize these words, Here are some Suggestions:\r\n"
-#        w = False
-#        for word in words:
-#            trans = self.Translate(word)
+   def TranslateSentense(self, sentense):
+       words = sentense.split(' ')
+       output = []
+       wrong = "Unable to recognize these words, Here are some Suggestions:\r\n"
+       w = False
+       for word in words:
+           trans = self.Translate(word)
             
-#            if trans.Translated():
-#                output.append(trans.Value)
-#            else:
-#                wrong += word + " => " + str(trans.Suggesions) + "\r\n"
-#                w = True
-#                output.append(word)
+           if trans.Translated():
+               output.append(trans.Value)
+           else:
+               wrong += word + " => " + str(trans.Suggesions) + "\r\n"
+               w = True
+               output.append(word)
 
-#        return " ".join(output) + "\r\n" + (wrong if w else "")
+       return " ".join(output) + "\r\n" + (wrong if w else "")
 
 
 
